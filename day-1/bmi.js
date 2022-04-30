0c17bb7919fed5c17f64737217ad236379cf2da8
henriqueRubido <henriquerubido1@gmail.com>
2022-04-30 18:29:24 -0300

    22.1 finished

--- day-1/bmi.js
+++ day-1/bmi.js
@@ -3,7 +3,6 @@
 // Normal weight = 18.5–24.9
 // Overweight = 25–29.9
 // Obesity = BMI of 30 or greater
-
 // BMI = kg/m2
 
 const readline = require('readline-sync');
@@ -12,7 +11,14 @@ const height = readline.questionInt('Qual sua altura? ');
 
 function calculateBMI(weight, height) {
   const bmi = (weight/ Math.pow(height/ 100,2)).toFixed(2);
-  console.log(bmi);
+  if (bmi < 18.5) {
+    return console.log(`${bmi} - Underweight`);
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    return console.log(`${bmi} - Normal Weight`);
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    return console.log(`${bmi} - Overweight`);
+  }
+  return console.log(`${bmi} - Obesity`);
 };
 
 calculateBMI(weight, height);
