fde38307ac3660ba2eabb63d439c9f352700164d
henriqueRubido <henriquerubido1@gmail.com>
2022-05-06 15:28:34 -0300

    simpsons exercise part 4

--- day-1/simpsons.js
+++ day-1/simpsons.js
@@ -7,12 +7,19 @@ async function main(id) {
   // part 1
   const readSimpsons = await fs.readFile('./simpsons.json', 'utf-8');
   const characters = JSON.parse(readSimpsons);
-  const filterChars = characters.filter(char => char.id !== '10' && char.id !== '6');
-  await fs.writeFile('./simpsons.json', JSON.stringify(filterChars));
+
+  // part 3
+  // const filterChars = characters.filter(({id}) => id !== '10' && id !== '6');
+  // await fs.writeFile('./simpsons.json', JSON.stringify(filterChars));
+
+  // part 4
+
+  const filterChars = characters.filter(({id}) => id < 5);
+  await fs.writeFile('./simpsonFamily.json', JSON.stringify(filterChars));
+  
+  //part 2
   const mapChars = filterChars.map(({id, name}) => `${id} - ${name}`);
   mapChars.forEach(char => console.log(char));
-
-  //part 2
   const simpsonId = characters.find((char) => Number(char.id) === id);
 
   if (simpsonId === undefined) {
