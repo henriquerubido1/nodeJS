3ebbc12c073a4b85800f2acd4f7783347b02621b
henriqueRubido <henriquerubido1@gmail.com>
2022-05-18 14:58:33 -0300

    minor changes

--- day-2/index.js
+++ day-2/index.js
@@ -1,11 +1,24 @@
+// Body-parser is the Node.js body parsing middleware. It is responsible
+// for parsing the incoming request bodies in a middleware before you handle it.
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+// Express' app.get() function lets you define a route handler for GET requests to
+// a given URL. For example, the below code registers a route handler that Express
+// will call when it receives an HTTP GET request to / .
+
 app.get('/', (req, res) => {
   res.send('hello world')
-})
+});
+
+// The app.use() function is used to mount the specified middleware
+// function(s) at the path which is being specified. It is mostly
+// used to set up middleware for your application.
+
+app.use(bodyParser.json());
 
 app.listen(4000, () => {
   console.log('PORT 4000');
--- day-3/index.js
+++ day-3/index.js
@@ -27,11 +27,17 @@ app.post('/greetings', (req, res) => {
   const { name, age } = req.body;
 
   if (parseInt(age, 10) <= 17) {
-    // não esqueça de adicionar o return para impedir de que seu código continue.
     return res.status(401).json({ message: `Unauthorized` });
   }
 
   return res.status(200).json({ message: `Hello, ${name}!` });
 });
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+
+  return res.status(200)
+    .json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade` });
+})
 
 app.listen(3001, () => console.log('ouvindo na porta 3001!'));
